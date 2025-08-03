8946f8dc91c5f58b73d536c0e4fa7c54da51d3dc
reza <moradzade.mohamadreza@gmail.com>
2025-08-02 21:24:49 -0700

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -12,23 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MainScreen(),
@@ -50,16 +39,273 @@ class _MainScreenState extends State<MainScreen> {
         ..setLooping(true)
         ..play();
 
+  Timer? timer;
+
+  bool showControlPanel = false;
+
   @override
   void dispose() {
     _controller.dispose();
+    timer?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [Positioned.fill(child: VideoPlayer(_controller))]),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: () {
+                if (!showControlPanel) {
+                  setState(() {
+                    showControlPanel = true;
+                  });
+                  initControlPanelTimer();
+                }
+              },
+              child: VideoPlayer(_controller),
+            ),
+          ),
+          if (showControlPanel)
+            VideoControlPanel(
+              controller: _controller,
+              gestureTapCallback: () {
+                setState(() {
+                  showControlPanel = false;
+                });
+                timer?.cancel();
+              },
+            ),
+        ],
+      ),
     );
+  }
+
+  void initControlPanelTimer() {
+    timer?.cancel();
+    timer = Timer(const Duration(seconds: 2), () {
+      setState(() {
+        showControlPanel = false;
+      });
+    });
+  }
+}
+
+class VideoControlPanel extends StatefulWidget {
+  const VideoControlPanel({
+    super.key,
+    required VideoPlayerController controller,
+    required this.gestureTapCallback,
+  }) : _controller = controller;
+
+  final VideoPlayerController _controller;
+  final GestureTapCallback gestureTapCallback;
+
+  @override
+  State<VideoControlPanel> createState() => _VideoControlPanelState();
+}
+
+class _VideoControlPanelState extends State<VideoControlPanel> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: GestureDetector(
+        onTap: widget.gestureTapCallback,
+        child: Container(
+          decoration: BoxDecoration(color: Colors.black.withAlpha(40)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, top: 48, right: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'James Bytheway',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      '@jamesbtw',
+                      style: TextStyle(color: Colors.black54, fontSize: 13),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'All The Damn Vampires',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'saturday',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 8),
+                    VideoProgressIndicator(
+                      widget._controller,
+                      allowScrubbing: true,
+                      colors: const VideoProgressColors(
+                        playedColor: Colors.white,
+                        backgroundColor: Colors.white10,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            widget._controller.value.position
+                                .toMinutesSeconds(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                          Text(
+                            widget._controller.value.duration
+                                .toMinutesSeconds(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              widget._controller.seekTo(
+                                Duration(
+                                  milliseconds:
+                                      widget
+                                          ._controller
+                                          .value
+                                          .position
+                                          .inMilliseconds -
+                                      2000,
+                                ),
+                              );
+                              setState(() {});
+                            },
+                            iconSize: 24,
+                            icon: const Icon(
+                              CupertinoIcons.backward_fill,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              if (widget._controller.value.isPlaying) {
+                                widget._controller.pause();
+                                setState(() {});
+                              } else {
+                                widget._controller.play();
+                                setState(() {});
+                              }
+                            },
+                            iconSize: 56,
+                            icon: Icon(
+                              widget._controller.value.isPlaying
+                                  ? CupertinoIcons.pause_circle_fill
+                                  : CupertinoIcons.play_circle_fill,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              widget._controller.seekTo(
+                                Duration(
+                                  milliseconds:
+                                      widget
+                                          ._controller
+                                          .value
+                                          .position
+                                          .inMilliseconds +
+                                      2000,
+                                ),
+                              );
+                              setState(() {});
+                            },
+                            iconSize: 24,
+                            icon: const Icon(
+                              CupertinoIcons.forward_fill,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+extension DurationExtensions on Duration {
+  /// Converts the duration into a readable string
+  /// 05:15
+  String toHoursMinutes() {
+    String twoDigitMinutes = _toTwoDigits(inMinutes.remainder(60));
+    return "${_toTwoDigits(inHours)}:$twoDigitMinutes";
+  }
+
+  /// Converts the duration into a readable string
+  /// 05:15:35
+  String toHoursMinutesSeconds() {
+    String twoDigitMinutes = _toTwoDigits(inMinutes.remainder(60));
+    String twoDigitSeconds = _toTwoDigits(inSeconds.remainder(60));
+    return "${_toTwoDigits(inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  String toMinutesSeconds() {
+    String twoDigitMinutes = _toTwoDigits(inMinutes.remainder(60));
+    String twoDigitSeconds = _toTwoDigits(inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  String _toTwoDigits(int n) {
+    if (n >= 10) return "$n";
+    return "0$n";
   }
 }
